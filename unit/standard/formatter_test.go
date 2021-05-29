7193eeac1af6a94f36089dbc31a0075f4a54ba2d
simp7 <JHParkOfficial@protonmail.com>
2021-05-29 19:50:37 +0900

    Add formatter_test.go

---
+++ unit/standard/formatter_test.go
@@ -0,0 +1,16 @@
+package standard
+
+import (
+	"github.com/simp7/nonogram/unit"
+	"testing"
+)
+
+func TestFormatter_Encode(t *testing.T) {
+	table := []struct {
+		input  unit.Map
+		output string
+	}{
+		{},
+		{},
+	}
+}
